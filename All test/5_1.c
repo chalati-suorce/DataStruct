20e0ac6d9414c3a41e3ceb667692ad3e3bc0ece8
chalatu <1841220662@qq.com>
2025-11-04 08:18:25 +0800

    "之前忘了comment"

---
+++ All test/5_1.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct BiTNode {
+    char data;
+    struct BiTNode *lchild, *rchild;
+} BiTNode, *BiTree;
+
+
+// 先序递归建立二叉树
+void CreateBiTree(BiTree *T) {
+    char ch;
+    scanf("%c", &ch);
+    
+    while (ch == ' ' || ch == '\n') {
+        scanf("%c", &ch);
+    }
+    
+    if (ch == '#') {
+        *T = NULL; 
+    } else {
+        *T = (BiTree)malloc(sizeof(BiTNode));
+        if (!*T) {
+            printf("内存分配失败！\n");
+            exit(1);
+        }
+        (*T)->data = ch;
+        CreateBiTree(&(*T)->lchild);  // 左子树
+        CreateBiTree(&(*T)->rchild);  // 右子树
+    }
+}
+
+// 先序遍历 ，根左右
+void PreOrderTraverse(BiTree T) {
+    if (T) {
+        printf("%c ", T->data);
+        PreOrderTraverse(T->lchild); 
+        PreOrderTraverse(T->rchild); 
+    }
+}
+
+// 中序遍历，左根右
+void InOrderTraverse(BiTree T) {
+    if (T) {
+        InOrderTraverse(T->lchild); 
+        printf("%c ", T->data); 
+        InOrderTraverse(T->rchild); 
+    }
+}
+
+// 后序遍历，左右根
+void PostOrderTraverse(BiTree T) {
+    if (T) {
+        PostOrderTraverse(T->lchild);
+        PostOrderTraverse(T->rchild);
+        printf("%c ", T->data); 
+    }
+}
+
+int main() {
+    BiTree T = NULL;
+    
+    printf("二叉树建立与遍历程序\n");
+    printf("请输入扩展先序遍历序列（用#表示空指针，回车结束输入）：\n");
+    printf("例如：ABD##E##CF##G##\n");
+    
+    CreateBiTree(&T);
+    
+    printf("\n遍历结果\n");
+    
+    printf("先序遍历结果：");
+    PreOrderTraverse(T);
+    printf("\n");
+    
+    printf("中序遍历结果：");
+    InOrderTraverse(T);
+    printf("\n");
+    
+    printf("后序遍历结果：");
+    PostOrderTraverse(T);
+    printf("\n");
+    
+    return 0;
+}
