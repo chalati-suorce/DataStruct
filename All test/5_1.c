30129b8bd5b9673e8e0e2ad99b67381ddb1511df
chalatu <1841220662@qq.com>
2025-11-04 09:01:05 +0800

    "test"

--- All test/5_1.c
+++ All test/5_1.c
@@ -59,6 +59,7 @@ void PostOrderTraverse(BiTree T) {
 
 int main() {
     BiTree T = NULL;
+    int choice;
     
     printf("二叉树建立与遍历程序\n");
     printf("请输入扩展先序遍历序列（用#表示空指针，回车结束输入）：\n");
@@ -66,19 +67,49 @@ int main() {
     
     CreateBiTree(&T);
     
-    printf("\n遍历结果\n");
+    printf("\n请选择遍历方式：\n");
+    printf("1. 先序遍历\n");
+    printf("2. 中序遍历\n");
+    printf("3. 后序遍历\n");
+    printf("4. 所有遍历方式\n");
+    printf("请输入选择（1-4）：");
+    scanf("%d", &choice);
     
-    printf("先序遍历结果：");
-    PreOrderTraverse(T);
-    printf("\n");
+    printf("\n遍历结果：\n");
     
-    printf("中序遍历结果：");
-    InOrderTraverse(T);
-    printf("\n");
-    
-    printf("后序遍历结果：");
-    PostOrderTraverse(T);
-    printf("\n");
+    switch (choice) {
+        case 1:
+            printf("先序遍历结果：");
+            PreOrderTraverse(T);
+            printf("\n");
+            break;
+        case 2:
+            printf("中序遍历结果：");
+            InOrderTraverse(T);
+            printf("\n");
+            break;
+        case 3:
+            printf("后序遍历结果：");
+            PostOrderTraverse(T);
+            printf("\n");
+            break;
+        case 4:
+            printf("先序遍历结果：");
+            PreOrderTraverse(T);
+            printf("\n");
+            
+            printf("中序遍历结果：");
+            InOrderTraverse(T);
+            printf("\n");
+            
+            printf("后序遍历结果：");
+            PostOrderTraverse(T);
+            printf("\n");
+            break;
+        default:
+            printf("无效选择！\n");
+            break;
+    }
     
     return 0;
 }
